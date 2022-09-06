0afd8379757c3318c64c0b4e8f44c20ca1bd483f
Kris <kharonek@yahoo.com>
2022-09-06 10:14:51 -0300

    Link fix #2

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,39 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Navbar() {
-	return (
-		<div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
-			<div className="max-w-[1000px] m-auto flex justify-between items-center p-4">
-				<h1 className="logo text-5xl">
-					<Link href="/">KMH</Link>
-				</h1>
+  return (
+    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
+      <div className="max-w-[1000px] m-auto flex justify-between items-center p-4">
+        <h1 className="logo text-5xl">
+          <Link href="/">KMH</Link>
+        </h1>
 
-				<ul className="flex text-2xl">
-					<Link href="/work">
-						<li className="p-4 hover:bg-orange-500 rounded-lg cursor-pointer ease-in duration-100">
-							work
-						</li>
-					</Link>
-					<Link href="/#about">
-						<li className="p-4 hover:bg-orange-500 rounded-lg cursor-pointer ease-in duration-100">
-							about
-						</li>
-					</Link>
-					<Link href="/contact">
-						<li className="p-4 hover:bg-orange-500 rounded-lg cursor-pointer ease-in duration-100">
-							contact
-						</li>
-					</Link>
-				</ul>
+        <ul className="flex text-2xl">
+          <li className="flex">
+            <Link href="/work">
+              <span className="p-4 hover:bg-orange-500 rounded-lg cursor-pointer ease-in duration-100">
+                work
+              </span>
+            </Link>
+          </li>
 
-			</div>
-		</div>
-	);
+          <li className="flex">
+            <Link href="/#about">
+              <span className="p-4 hover:bg-orange-500 rounded-lg cursor-pointer ease-in duration-100">
+                about
+              </span>
+            </Link>
+          </li>
+
+          <Link href="/contact">
+            <li className="flex">
+              <span className="p-4 hover:bg-orange-500 rounded-lg cursor-pointer ease-in duration-100">
+                contact
+              </span>
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
